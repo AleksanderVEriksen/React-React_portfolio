8979787b723478586af28aff3027d218d5e1075e
AleksanderVEriksen <70018977+AleksanderVEriksen@users.noreply.github.com>
2025-02-22 14:19:00 +0100

    c

--- vite.config.js
+++ vite.config.js
@@ -3,33 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  
-
-  esbuild: {
-    loader: "jsx",
-    include: /src\/.*\.jsx?$/,
-    // loader: "tsx",
-    // include: /src\/.*\.[tj]sx?$/,
-    exclude: [],
-  },
-
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [react(),
-        {
-          name: "load-js-files-as-jsx",
-          setup(build) {
-            build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => ({
-              loader: "jsx",
-              contents: await fs.readFile(args.path, "utf8"),
-            }));
-          },
-        },
-      ],
-    },
-  },
   // Need base for images
   base: "/React-React_portfolio/",
+  plugins: [react()],
   css: {
     modules: {
       localsConvention: "camelCase"
