4cfb04e59fa790f28b9eb65dd0c0bf6f75590717
AleksanderVEriksen <70018977+AleksanderVEriksen@users.noreply.github.com>
2025-02-22 02:32:30 +0100

    comment for base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Need base for images
   base: "/React-React_portfolio/",
   plugins: [react()],
   css: {
