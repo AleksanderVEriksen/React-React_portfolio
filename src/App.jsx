ca916510f82d1c3b4913c2127ca5cc78f0ba17bf
AleksanderVEriksen <70018977+AleksanderVEriksen@users.noreply.github.com>
2025-02-22 02:00:27 +0100

    c

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,8 @@ import styles from './App.module.css'
 import { Navbar } from './components/Navbar/Navbar'
 import { Profile } from './components/Profile/Profile.jsx'
 import { About } from './components/About/About.jsx'
-
 import { Experience } from './components/Experience/Experience.jsx'
 import { Projects } from './components/Projects/Projects.jsx'
-
 import { Footer } from './components/Footer/Footer.jsx'
 
 function App() {
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/React-React_portfolio/",
+  //base: "/React-React_portfolio/",
   plugins: [react()],
   css: {
     modules: {
