f3dd9f2543a3ae9877edfd89d85061a2e44945f6
AleksanderVEriksen <70018977+AleksanderVEriksen@users.noreply.github.com>
2026-01-31 00:08:13 +0100

    Remove About component import from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import styles from './App.module.css'
 import { Navbar } from './components/Navbar/Navbar'
 import { Profile } from './components/Profile/Profile.jsx'
-import { About } from './components/About/About.jsx'
 import { Experience } from './components/Experience/Experience.jsx'
 import { Projects } from './components/Projects/Projects.jsx'
 import { Footer } from './components/Footer/Footer.jsx'
